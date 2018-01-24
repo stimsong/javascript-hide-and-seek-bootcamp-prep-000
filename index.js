e5c082ef15a24bdfc98f1862c335188476855ba0
Greg Stimson <stimsong@yahoo.com>
2018-01-24 22:26:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+// Hide And Seek Lab
+
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  const target = document.querySelector('div#nested .target');
+   return target;
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for ( let i = 0; i<lis.length; i++ ) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
